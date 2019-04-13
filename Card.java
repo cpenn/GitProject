4a212d3c6d962bd04c0cc344722a23b4cbea1e88
cpenn <chelsea1@seas.upenn.edu>
2019-04-13 16:12:08 +0930

    card class to be used in Blackjack

---
+++ Card.java
@@ -0,0 +1,24 @@
+
+public class Card {
+
+	String suit;
+	String rank;
+
+	// Constructor
+	Card(String st, String rk) {
+		this.suit = st;
+		this.rank = rk;
+	}
+
+	// Method: Display card
+	String DisplayCard() {
+		return rank + suit.charAt(0);
+	}
+
+//	public static void main(String[] args) {
+//		Card c = new Card("Heart", "A"); 
+//		System.out.println(c.DisplayCard()); 
+//		
+//	}
+
+}
